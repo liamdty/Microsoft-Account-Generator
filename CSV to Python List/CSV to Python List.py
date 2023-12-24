91cfd49589f7537216c1de4f16a65a0dee279c5a
liamdty <153718890+liamdty@users.noreply.github.com>
2023-12-24 18:57:47 -0500

    Update CSV to Python List.py

--- CSV to Python List/CSV to Python List.py
+++ CSV to Python List/CSV to Python List.py
@@ -1,5 +1,5 @@
-#This is a tool to simply convert a profile spreadsheet to the format needed for the accountgenerator
-#Use provided format in CSV.csv to provide desired profiles
+#This is a tool to simply convert a profile spreadsheet to the format needed for the account generator
+#Use the provided format in CSV.csv to provide desired profiles
 import csv
 
 with open('CSV.csv', newline='') as f:
