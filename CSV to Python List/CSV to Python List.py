cf8954dffc3e3cb90523ad463c2e5c6f2f39e503
liamdty <153718890+liamdty@users.noreply.github.com>
2023-12-24 18:29:23 -0500

    Add files via upload

---
+++ CSV to Python List/CSV to Python List.py
@@ -0,0 +1,8 @@
+#This is a tool to simply convert a profile spreadsheet to the format needed for the accountgenerator
+#Use provided format in CSV.csv to provide desired profiles
+import csv
+
+with open('CSV.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+print(data)
